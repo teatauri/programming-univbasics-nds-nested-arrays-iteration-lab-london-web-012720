e51faf64571b917bb3cb1d4f4b09c53a6f9088a1
Lee Bardon <leerbardon@gmail.com>
2019-12-22 19:29:17 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -47,7 +47,7 @@ row_index = 0
   while row_index < src.length do 
     if src[row_index][0] %2 == 0 
       if src[row_index][1] %2 == 0 
-        total << src[row_index]
+        total << src[row_index][0] + src[row_index][1]
       end 
     end
     row_index += 1 
